432ce10b8615647d12d079ce09a198405f9f8f7c
JKD0400 <82791935+JKD0400@users.noreply.github.com>
2024-05-13 13:03:09 +0530

    chatbot_stream.py

--- chatbot_stream.py
+++ chatbot_stream.py
@@ -35,24 +35,32 @@ def load_data():
 
 # Function to create the language model and embedder (replace placeholders)
 def create_llm_and_embedder():
-    llm = LlamaCPP(
-        # You can pass in the URL to a GGML model to download it automatically
-        model_url="https://huggingface.co/TheBloke/Llama-2-13B-chat-GGML/resolve/main/llama-2-13b-chat.ggmlv3.q4_0.bin",
-        # optionally, you can set the path to a pre-downloaded model instead of model_url
-        model_path=None,
-        temperature=0.1,
-        max_new_tokens=256,
-        # llama2 has a context window of 4096 tokens, but we set it lower to allow for some wiggle room
-        context_window=3900,
-        # kwargs to pass to __call__()
-        generate_kwargs={},
-        # kwargs to pass to __init__()
-        # set to at least 1 to use GPU
-        model_kwargs={"n_gpu_layers": 30},
-        # transform inputs into Llama2 format
-        messages_to_prompt=messages_to_prompt,
-        completion_to_prompt=completion_to_prompt,
-        verbose=True,
+    # llm = LlamaCPP(
+    #     # You can pass in the URL to a GGML model to download it automatically
+    #     model_url="https://huggingface.co/TheBloke/Llama-2-13B-chat-GGML/resolve/main/llama-2-13b-chat.ggmlv3.q4_0.bin",
+    #     # optionally, you can set the path to a pre-downloaded model instead of model_url
+    #     model_path=None,
+    #     temperature=0.1,
+    #     max_new_tokens=256,
+    #     # llama2 has a context window of 4096 tokens, but we set it lower to allow for some wiggle room
+    #     context_window=3900,
+    #     # kwargs to pass to __call__()
+    #     generate_kwargs={},
+    #     # kwargs to pass to __init__()
+    #     # set to at least 1 to use GPU
+    #     model_kwargs={"n_gpu_layers": 30},
+    #     # transform inputs into Llama2 format
+    #     messages_to_prompt=messages_to_prompt,
+    #     completion_to_prompt=completion_to_prompt,
+    #     verbose=True,
+    # )
+    llm = LlamaCpp(
+    model_path="/Users/rlm/Desktop/Code/llama.cpp/models/openorca-platypus2-13b.gguf.q4_0.bin",
+    temperature=0.75,
+    max_tokens=2000,
+    top_p=1,
+    callback_manager=callback_manager,
+    verbose=True,  # Verbose is required to pass to the callback manager
     )
 
     # embed_model = LangchainEmbedding(
