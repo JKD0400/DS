83128d405bcb3c5add8fbcbd579f0f8dbf171cc2
JKD0400 <82791935+JKD0400@users.noreply.github.com>
2024-05-13 13:19:56 +0530

    chatbot_stream.py

--- chatbot_stream.py
+++ chatbot_stream.py
@@ -23,6 +23,7 @@ import torch
 import json
 import os
 
+model_url = "https://huggingface.co/TheBloke/Llama-2-13B-chat-GGML/resolve/main/llama-2-13b-chat.ggmlv3.q4_0.bin"
 # Function to configure logging (optional)
 def configure_logging():
     logging.basicConfig(stream=sys.stdout, level=logging.INFO)
@@ -35,9 +36,28 @@ def load_data():
 
 # Function to create the language model and embedder (replace placeholders)
 def create_llm_and_embedder():
+    # llm = LlamaCPP(
+    #     # You can pass in the URL to a GGML model to download it automatically
+    #     model_url="https://thebloke/Llama-2-7B-GGUF",
+    #     # optionally, you can set the path to a pre-downloaded model instead of model_url
+    #     model_path=None,
+    #     temperature=0.1,
+    #     max_new_tokens=256,
+    #     # llama2 has a context window of 4096 tokens, but we set it lower to allow for some wiggle room
+    #     context_window=3900,
+    #     # kwargs to pass to __call__()
+    #     generate_kwargs={},
+    #     # kwargs to pass to __init__()
+    #     # set to at least 1 to use GPU
+    #     model_kwargs={"n_gpu_layers": 30},
+    #     # transform inputs into Llama2 format
+    #     messages_to_prompt=messages_to_prompt,
+    #     completion_to_prompt=completion_to_prompt,
+    #     verbose=True,
+    # )
     llm = LlamaCPP(
         # You can pass in the URL to a GGML model to download it automatically
-        model_url="https://thebloke/Llama-2-7B-GGUF",
+        model_url=model_url,
         # optionally, you can set the path to a pre-downloaded model instead of model_url
         model_path=None,
         temperature=0.1,
@@ -48,7 +68,7 @@ def create_llm_and_embedder():
         generate_kwargs={},
         # kwargs to pass to __init__()
         # set to at least 1 to use GPU
-        model_kwargs={"n_gpu_layers": 30},
+        model_kwargs={"n_gpu_layers": 1},
         # transform inputs into Llama2 format
         messages_to_prompt=messages_to_prompt,
         completion_to_prompt=completion_to_prompt,
